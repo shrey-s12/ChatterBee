74fc997f57c47a24cd35f1869b7e5b077646378c
Shrey Singhal <shreynbd12@gmail.com>
2025-04-03 13:05:50 +0530

    Comment the Setting button in navbar... It will be completed soon

--- frontend-chatterbee/src/components/Navbar.jsx
+++ frontend-chatterbee/src/components/Navbar.jsx
@@ -19,10 +19,10 @@ const Navbar = () => {
           </div>
 
           <div className='flex items-center gap-2'>
-            <Link to='/settings' className='btn btn-sm gap-2 transition-colors'>
+            {/* <Link to='/settings' className='btn btn-sm gap-2 transition-colors'>
               <Settings className='w-4 h-4' />
               <span className='hidden sm:inline'>Settings</span>
-            </Link>
+            </Link> */}
 
             {authUser && (
               <>
